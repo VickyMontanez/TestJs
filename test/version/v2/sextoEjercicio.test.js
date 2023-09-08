e4bfe5ea2ac017346701194e739fe836647a74f0
VickyMontanezCampus <vmontanez707@gmail.com>
2023-09-08 15:58:51 -0500

    fix: :pencil2: Fix a typo in the Sixth test and function
    Fix a typo in the sixth test and function

--- test/version/v2/sextoEjercicio.test.js
+++ test/version/v2/sextoEjercicio.test.js
@@ -5,7 +5,7 @@ const generos = ["M", "F", "M"];
 const notas = [85, 92, 78];
 
 describe("La función debe el nombre del estudiante con la nota más alta y más baja, además de la cantidad de hombre y mujeres", () => {
-    test("La función debe devolver el nombre del estudiante con la nota más alta", () => {
+    test("La función debe el nombre del estudiante con la nota más alta y más baja, además de la cantidad de hombre y mujeres", () => {
         const app = procesarEstudiantes(nombres, generos, notas)
         expect(app).toBe(`El Estudiante con la nota más alta es Estudiante2 con 92; El Estudiante con la nota más baja es Estudiante3 con 78. La cantidad de hombres es 2 y de mujeres es 1`)
     })
--- version/v2/sextoEjercicio.js
+++ version/v2/sextoEjercicio.js
@@ -2,26 +2,6 @@
 /* 6.Construir el algoritmo en Javascript para un programa para cualquier cantidad de estudiantes que lea el nombre, el sexo y la nota definitiva y halle 
 al estudiante con la mayor nota y al estudiante con la menor nota y cuantos eran hombres y cuantos mujeres. */
 
-/* export const managmentSchool = (name, genre, finalRating) => {
-    const size = name.length;
-    if (genre.length !== size || finalRating.length !== size) return "You're missing data";
-
-    if (!finalRating.every((value) => typeof value === "number")) return "finalRating should be a number"
-
-    const bestRating = name[finalRating.indexOf(Math.max(...finalRating))];
-    const lowestRating = name[finalRating.indexOf(Math.min(...finalRating))];
-
-    let { men, woman } = genre.reduce((count, val) => {
-        if (val === "M") count.men++; else if (val === "W") count.woman++;
-        return count;
-    }, { men: 0, woman: 0 })
-
-    if (genre.length !== men + woman) return "Only 'M' or 'W' as values of genres"
-
-    const result = `The best rating is ${bestRating} and the lowest rating is ${lowestRating}. There are ${men} men and ${woman} woman`;
-    return result;
-} */
-
 export const procesarEstudiantes = (nombre, genero, nota) => {
     let maxNota;
     let minNota
